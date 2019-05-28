6e6d725bc16c1ee46c3fd5d10b4c96269fdde17e
1141932767 <51064677+1141932767@users.noreply.github.com>
2019-05-28 11:42:36 +0800

    ignore

--- src/com/jie/text/App.java
+++ src/com/jie/text/App.java
@@ -3,6 +3,6 @@ package com.jie.text;
 public class App {
     public static void main(String[] args) {
         System.out.println("hahah");
-        System.out.println("kkkkkk");
+       // System.out.println("kkkkkk");
     }
 }
