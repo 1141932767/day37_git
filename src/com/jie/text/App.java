59bd830853420727d4bbaf6a9c9deded36b289bd
jie <123444@aliyun.com>
2019-05-28 11:48:41 +0800

    change add

--- src/com/jie/text/App.java
+++ src/com/jie/text/App.java
@@ -2,7 +2,8 @@ package com.jie.text;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hahah");
+        System.out.println("wenwen");
        // System.out.println("kkkkkk");
+        int a = 10;
     }
 }
