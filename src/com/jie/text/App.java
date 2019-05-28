db7f19e58838e2d28b1bd54879b4adb93fb6e7ca
1141932767 <51064677+1141932767@users.noreply.github.com>
2019-05-28 11:47:13 +0800

    change

--- src/com/jie/text/App.java
+++ src/com/jie/text/App.java
@@ -3,6 +3,9 @@ package com.jie.text;
 public class App {
     public static void main(String[] args) {
         System.out.println("hahah");
-       // System.out.println("kkkkkk");
+        System.out.println("kkkkkk");
+        System.out.println("kkk");
+        System.out.println("hhkkk");
+        System.out.println("kkrrk");
     }
 }
